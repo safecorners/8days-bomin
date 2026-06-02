a27fc1410f155f7d0fa8437ce72106a40cb10333
safecorners <safecorners.mobile@gmail.com>
2026-06-02 10:31:40 +0900

    사용하지 않는 코드 제거

--- app.py
+++ app.py
@@ -1,44 +1,13 @@
 import streamlit as st
 import serial
 
-from google import genai
-from google.genai import types
-
 from datetime import datetime
 import time
 import json
 
-import os
-from dotenv import load_dotenv
-
-
 # =========================================================
-# [구역 1] 환경 설정
+# 아두이노 연결
 # =========================================================
-
-load_dotenv()
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-MODEL_NAME = "gemini-2.5-flash"
-
-st.set_page_config(page_title="8일간의 아두이노", layout="wide")
-
-
-# =========================================================
-# [구역 2] 리소스 및 외부 연결 관리
-# =========================================================
-
-@st.cache_resource
-def get_client():
-    load_dotenv()
-    api_key = os.getenv("GEMINI_API_KEY")
-
-    if not api_key:
-        st.error("🔑API 키가 설정되지 않았습니다.")
-        st.stop()
-
-    return genai.Client(api_key=api_key)
-
-client = get_client()
 
 @st.cache_resource
 def get_ser(port):
@@ -58,65 +27,23 @@ else:
 
 
 # =========================================================
-# [구역 3] 상태 초기화
+# 상태 초기화
 # =========================================================
 
 if "raw_data" not in st.session_state:
     st.session_state.raw_data = []
 
-if "angle" not in st.session_state:
-    st.session_state.angle = 90
-
-if "last_sent_angle" not in st.session_state:
-    st.session_state.last_sent_angle = 90
-
-if "current_light" not in st.session_state:
-    st.session_state.current_light = 0
-
-
 # =========================================================
-# [구역 4] AI 에이전트 및 도구(Tools) 정의
-# =========================================================
-
-def load_system_prompt(file_name):
-    file_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(file_dir, file_name)
-    try:
-        with open(file_path, "r", encoding="utf-8") as f:
-            return f.read()
-    except FileNotFoundError:
-        st.error(f"{file_path}를 찾을 수 없습니다.")
-        st.stop()
-
-if "chat_session" not in st.session_state:
-    system_prompt = load_system_prompt("system_prompt.md")
-    tool_list = []
-    st.session_state.chat_session = client.chats.create(
-        model=MODEL_NAME,
-        config= types.GenerateContentConfig(
-            system_instruction=system_prompt,
-            tools= tool_list,
-            automatic_function_calling=types.AutomaticFunctionCallingConfig(
-                disable=False,
-                maximum_remote_calls=7,
-            )
-        ),
-    )
-
-
-# =========================================================
-# [구역 5] 데이터 수집
+# 데이터 수집
 # =========================================================
 
 
 # =========================================================
-# [구역 6] 페이지 내비게이션 및 앱 실행
+# 페이지 내비게이션 및 앱 실행
 # =========================================================
 
 pages = [
     st.Page("dashboard.py", title="대시보드", icon=":material/dashboard:", default=True),
-    st.Page("chatbot.py", title="챗봇", icon=":material/smart_toy:"),
-    st.Page("control.py", title="수동 제어", icon=":material/adjust:"),
 ]
 
 page = st.navigation(pages=pages)
